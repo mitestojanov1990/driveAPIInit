d1add28dcd6e75479e80bedfe493245a879f054f
Borko Spasic <borko@miraclemill.com>
2016-02-08 17:44:06 +0100

    no message

--- Gdrive/app.js
+++ Gdrive/app.js
@@ -1,4 +1,39 @@
 ﻿var fs = require('fs');
+var request = require('request');
+var restify = require('restify');
+var https = require('https');
+var http = require('http');
+var server = restify.createServer();
+
+restify.CORS.ALLOW_HEADERS.push('authorization');
+
+server.use(restify.CORS());
+server.use(restify.fullResponse());
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.jsonp());
+server.use(restify.bodyParser({ mapParams: false }));
+server.use(restify.queryParser());
+
+// Add headers
+server.use(function (req, res, next) {
+    SetCORSHeaders(res);
+    next();
+});
+
+function SetCORSHeaders(res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-Requested-With, X-PINGOTHER, X-CSRF-Token,Authorization');
+    
+    //added
+    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+    res.setHeader('Access-Control-Max-Age', '1000');
+    //added
+    
+    res.setHeader('Access-Control-Allow-Credentials', true);
+}
+
 var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
@@ -102,7 +137,8 @@ var service = google.drive('v2');
 
 var rootDirName = 'apiTest';
 
-var emptyDirName = 'emptyFolder';
+var Gauth = null;
+var gotData = false;
 
 var templateObj = {};
 var contractObj = {};
@@ -111,18 +147,19 @@ var templateDirName = 'template';
 var contractDirName = 'contract';
 
 var templateChildrens = [];
+var templatesData = false;
 var contractChildrens = [];
-
-var tmpFile = {};
+var contractData = false;
 
 
 // works - file listing with query
 function getAllFiles(auth, query, callback) {
-    service.files.list({
+   service.files.list({
         auth: auth,
-        q: query
+        q: query,
+        maxResults: 1000
     }, function (err, response) {
-        var resp = {error: null, success: null};
+        var resp = new ResultObject();
         if (err) {
             console.log('The API returned an error: ' + err);
             resp.error = err;
@@ -146,8 +183,21 @@ function getAllFiles(auth, query, callback) {
                 
 
                 if (contractObj.id != undefined && templateObj.id != undefined) {
-                    //copyFile(auth, tmpFile.id, contractObj.id, "test123");
+                    gotData = true;
                     break;
+                }
+            }
+            
+            for (var i = 0; i < items.length; i++) {
+                var item = items[i];
+                if (item.labels.trashed)
+                    continue;
+                if (item.parents.length > 0) {
+                    var parentId = item.parents[0].id;
+
+                    if (parentId == templateObj.id) {
+                        templateChildrens.push(item);
+                    }
                 }
             }
         }
@@ -155,26 +205,25 @@ function getAllFiles(auth, query, callback) {
     });
 }
 
-function getTemplateChildrens(auth, parentId) {
+function getTemplateChildrens(auth, parentId, callback) {
     getChildrens(auth, parentId, function (res) {
-        if (res.error != null) { 
-        
-        } else { 
+        if (res.error == null) {
+            console.log('Got childrens.');
+            templatesData = true;
             templateChildrens = res.success;
-            console.log('template childrens found.');
         }
+        callback(res);
     });
 }
 
-function getContractChildrens(auth, parentId) {
+function getContractChildrens(auth, parentId, callback) {
     getChildrens(auth, parentId, function (res) {
-        if (res.error != null) { 
-        
-        } else {
+        if (res.error == null) {
+            console.log('Got childrens.');
+            contractData = true;
             contractChildrens = res.success;
-            console.log('contract childrens found.');
-
         }
+        callback(res);
     });
 }
 
@@ -184,7 +233,7 @@ function getChildrens(auth, parentId, callback) {
         auth: auth,
         folderId: parentId
     }, function (err, response) {
-        var resp = {error: null, success: null};
+        var resp = new ResultObject();
         if (err) {
             console.log('The API returned an error: ' + err);
             resp.error = err;
@@ -196,21 +245,37 @@ function getChildrens(auth, parentId, callback) {
     });
 }
 
-// works - can't set metadata
-function addFileInFolder(auth, parentId, folderName) {
-    var body = { title: folderName, mimeTyle: 'application/vnd.google-apps.folder', parents: [{ id: parentId }] };
+function getFilesFromFolder() { 
 
-    service.files.insert({
-        auth: auth,
-        resource: body
-    }, function (err, response) {
-        if (err) {
-            console.log('The API returned an error: ' + err);
-            return;
+}
+
+// works
+function addFileInFolder(auth, parentId, folderName, callback) {
+    var fullUrl = "https://www.googleapis.com/drive/v2/files";
+    
+    var headers = { 'Authorization': auth.credentials.token_type + ' ' + auth.credentials.access_token };
+    
+    var bodyData = {
+        "title": folderName,
+        "parents": [{ "id": parentId }],
+        "mimeType": "application/vnd.google-apps.folder"
+    };
+    
+    var post_options = {
+        method: 'POST',
+        url: fullUrl,
+        json: bodyData,
+        headers: headers
+    };
+    
+    request(post_options, function (error, response, body) {
+        var resp = new ResultObject();
+        if (error != null) { 
+            resp.error = error;
         } else {
-            console.log('file created.');
-
+            resp.success = body;
         }
+        callback(body);
     });
 }
 
@@ -241,10 +306,10 @@ function updateFolderMetadata(auth, fileId) {
 }
 
 // works - copy file from destination with specific filename
-function copyFile(auth, sourceFileId, targetFileId, fileName) {
+function copyFile(auth, sourceFileId, targetFileId, fileName, callback) {
     getFileById(auth, sourceFileId, function (response) {
-        if (response.error != null) { 
-        
+        if (response.error != null) {
+            callback(response);
         } else {
             var newFile = response.success;
             newFile.parents = [{ id: targetFileId }];
@@ -254,11 +319,15 @@ function copyFile(auth, sourceFileId, targetFileId, fileName) {
                 fileId: sourceFileId,
                 resource: newFile
             }, function (err, response) {
+                var resp = new ResultObject();
                 if (err) {
                     console.log('The API returned an error: ' + err);
+                    resp.error = err;
                 } else {
                     console.log("File copied!");
+                    resp.success = response;
                 }
+                callback(resp);
             });
         }
     });
@@ -271,7 +340,7 @@ function getChildrenByParentAndId(auth, parentId, childId, callback) {
         folderId: parentId,
         childId: childId
     }, function (err, response) {
-        var resp = { error: null, success: null };
+        var resp = new ResultObject();
         if (err) {
             console.log('The API returned an error: ' + err);
             resp.error = err;
@@ -290,7 +359,7 @@ function getFileById(auth, fileId, callback) {
         auth: auth,
         fileId: fileId
     }, function (err, response) {
-        var resp = { error: null, success: null };
+        var resp = new ResultObject();
         if (err) {
             console.log('The API returned an error: ' + err);
             resp.error = err;
@@ -308,18 +377,173 @@ function onAPIReady(auth) {
     var query = "mimeType='application/vnd.google-apps.folder'";
     
     query = "";
+    
+    Gauth = auth;
 
     getAllFiles(auth, query, function (res) {
         if (res.error != null) { 
         
         } else {
-            getTemplateChildrens(auth, templateObj.id);
-            getContractChildrens(auth, templateObj.id);
+            //getTemplateChildrens(auth, templateObj.id, function (result) { });
+            getContractChildrens(auth, templateObj.id, function (result) { });
         }
     });
 
-    //updateFolderMetadata(auth, "");
+}
 
-    //addFileInFolder(auth, contractObj.id, "mite-testing");
+// Get Templates
+server.get('/gdrive/gettemplates', function (req, res, next) {
+    if (CheckForData()) {
+        res.end(JSON.stringify(templateChildrens));
+        return next();
 
+    } else {
+        getAllFiles(Gauth, "", function (result) {
+            if (result.error != null) {
+                res.statusCode = 409;
+                res.end(result.error);
+                return next();
+            } else {
+                res.end(JSON.stringify(templateChildrens));
+                return next();
+            }
+        });
+    }
+});
+
+// Get Files From Contract Folder
+server.get('/gdrive/files/list', function (req, res, next) {
+    if (CheckForData()) {
+        if (contractData == false) {
+            getContractChildrens(Gauth, templateObj.id, function (result) {
+                if (result.error != null) {
+                    res.statusCode = 409;
+                    res.end(result.error);
+                    return next();
+                } else {
+                    res.end(JSON.stringify(result.success));
+                    return next();
+                }
+            });
+        } else {
+            res.end(JSON.stringify(contractChildrens));
+            return next();
+        }
+    } else {
+        getAllFiles(Gauth, "", function (result) {
+            if (result.error != null) {
+                res.statusCode = 409;
+                res.end(result.error);
+                return next();
+            } else {
+                getContractChildrens(Gauth, templateObj.id, function (result) {
+                    if (result.error != null) {
+                        res.statusCode = 409;
+                        res.end(result.error);
+                        return next();
+                    } else {
+                        res.end(JSON.stringify(result.success));
+                        return next();
+                    }
+                });
+                return next();
+            }
+        });
+    }
+});
+
+// Create Folder in Contract Folder
+server.post('/gdrive/folder/create/:contractid', function (req, res, next) {
+    if (CheckForData()) {
+        var result = new ResultObject();
+        if (IsNullOrEmpty(req.params.contractid) == false) {
+            var contractid = req.params.contractid;
+            
+            var filename = "Contract" + contractid;
+
+            addFileInFolder(Gauth, contractObj.id, filename, function (result) {
+                if (result.error != null) {
+                    res.statusCode = 409;
+                    res.end("Error creating folder");
+                    return next();
+                } else {
+                    res.end(JSON.stringify(result.success));
+                    return next();
+                }
+            });
+        }
+        else {
+            res.statusCode = 409;
+            res.end("Missing parameters");
+            return next();
+        }
+    } else {
+        res.statusCode = 409;
+        res.end("Couldn't load Google Drive Data");
+        return next();
+    }
+});
+
+// Copy File
+server.post('/gdrive/files/copy/:templateid/:contractid/:filename', function (req, res, next) {
+    if (CheckForData()) {
+        var result = new ResultObject();
+        if (IsNullOrEmpty(req.params.templateid) == false && IsNullOrEmpty(req.params.contractid) == false && IsNullOrEmpty(req.params.filename) == false) {
+            var templateid = req.params.templateid;
+            var contractid = req.params.contractid;
+            var filename = req.params.filename;
+            
+            copyFile(Gauth, templateid, contractid, filename, function (result) {
+                if (result.error != null) {
+                    res.statusCode = 409;
+                    res.end("Error copying file");
+                    return next();
+                } else {
+                    res.end(JSON.stringify(result.success));
+                    return next();
+                }
+            });
+        }
+        else {
+            res.statusCode = 409;
+            res.end("Missing parameters");
+            return next();
+        }
+    } else {
+        res.statusCode = 409;
+        res.end("Couldn't load Google Drive Data");
+        return next();
+    }
+});
+
+// PDF
+server.post('/gdrive/files/pdf/:documentid/:contractid', function (req, res, next) {
+    return next();
+});
+
+server.listen(3000, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+
+function ResultObject() {
+    this.success = null;
+    this.error = null;
+};
+
+function CheckForData() {
+    if (gotData)
+        return true;
+    else
+        return false;
+}
+
+function IsNullOrEmpty(value) {
+    if (!value || 0 === value.length) {
+        return true;
+    }
+    if (value === "null") {
+        return true;
+    }
+    return false;
 }
